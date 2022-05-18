73effc1599fadb9857c465442a7f1d99006ec96a
Facundo Dominguez <l.facundodominguez@gmail.com>
2022-05-18 13:43:20 -0300

    first commit

---
+++ ejerciciosCurso/ciclos/js/app.js
@@ -0,0 +1,30 @@
+const OBJETIVO = 15;
+let salesNumber;
+let salesman;
+let cycleEnds = false;
+
+salesman = prompt ('Ingrese el nombre del vendedor: ');
+salesNumber = prompt ('Ingrese sus ventas: ')
+
+while (!cycleEnds) {
+    if (salesNumber < OBJETIVO) {
+        alert('Hola '+ salesman + '! vas por la venta '+ salesNumber +' todavía no llegaste al objetivo.')
+    } else {
+            alert('Felicidades '+ salesman +' llegaste al objetivo! tenes ' + salesNumber +' ventas!')
+    }
+    respuestaVendedor = prompt('queres preguntar por otro vendedor? (ingresa si o no)').toLowerCase()
+    if (respuestaVendedor == 'si') {
+        salesman = prompt ('Ingrese el nombre del vendedor: ');
+        salesNumber = prompt ('Ingrese sus ventas: ')
+    } else {
+        if (respuestaVendedor == 'no') {
+            respuestaVentas = prompt ('Desea actualizar su numero de ventas? (ingresa si o no)').toLowerCase()
+            if (respuestaVentas == 'si') {
+                salesNumber = prompt ('Ingrese sus ventas: ')
+            } else {
+                cycleEnds = true;
+                alert('gracias, vuelva prontos! ')
+            }
+        }
+    }
+}
