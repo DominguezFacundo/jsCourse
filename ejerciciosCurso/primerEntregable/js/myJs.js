ad127e2856c89aa492fc2ec0f7a3978ac4043983
Facundo Dominguez <l.facundodominguez@gmail.com>
2022-05-28 01:39:12 -0300

    adding arrays

--- ejerciciosCurso/primerEntregable/js/myJs.js
+++ ejerciciosCurso/primerEntregable/js/myJs.js
@@ -1,59 +1,34 @@
-let day = parseInt(
-  prompt("(Siendo el mes de 31 días) Ingrese un numero del mes: ")
-);
+let lunes = [1, 8, 15, 22, 29];
+let martes = [2, 9, 16, 23, 30];
+let miercoles = [3, 10, 17, 24, 31];
+let jueves = [4, 11, 18, 25];
+let viernes = [5, 12, 19, 26];
+let sabado = [6, 13, 20, 27];
+let domingo = [7, 14, 21, 28];
 
-function pillbox(day) {  
-  switch (day) {
-    case 1:
-    case 8:
-    case 15:
-    case 22:
-    case 29:
-      return alert("Es lunes, usted debe tomar las pastillas: blancas");
-    case 2:
-    case 9:
-    case 16:
-    case 23:
-    case 30:
-      return alert("Es martes, usted debe tomar las pastillas: rojas");
-    case 3:
-    case 10:
-    case 17:
-    case 24:
-    case 31:
-      return alert("Es miércoles, usted debe tomar las pastillas: azules");
-    case 4:
-    case 11:
-    case 18:
-    case 25:
-      return alert("Es jueves, usted debe tomar las pastillas: rosas");
-    case 5:
-    case 12:
-    case 19:
-    case 26:
-      return alert(
-        "Es viernes, usted debe tomar las pastillas: blancas y rojas"
-      );
-    case 6:
-    case 13:
-    case 20:
-    case 27:
-      return alert(
-        "Es sábado, usted debe tomar las pastillas: azules y verdes"
-      );
-    case 7:
-    case 14:
-    case 21:
-    case 28:
-      return alert("Es domingo, usted no debe tomar nada hoy!");
+function pillbox() {
+  let dayOfWeek = parseInt(
+    prompt("(Siendo el mes de 31 días) Ingrese un numero del mes: ")
+  );
+  if (lunes.includes(dayOfWeek)) {
+    alert("Es lunes, usted debe tomar las pastillas: blancas");
+  } else if (martes.includes(dayOfWeek)) {
+    alert("Es martes, usted debe tomar las pastillas: rojas");
+  } else if (miercoles.includes(dayOfWeek)) {
+    alert("Es miércoles, usted debe tomar las pastillas: azules");
+  } else if (jueves.includes(dayOfWeek)) {
+    alert("Es jueves, usted debe tomar las pastillas: rosas");
+  } else if (viernes.includes(dayOfWeek)) {
+    alert("Es viernes, usted debe tomar las pastillas: blancas y rojas");
+  } else if (sabado.includes(dayOfWeek)) {
+    alert("Es sábado, usted debe tomar las pastillas: azules y verdes");
+  } else if (domingo.includes(dayOfWeek)) {
+    alert("Es domingo, usted no debe tomar nada hoy!");
+  } else {
+    alert("Dia incorrecto, por favor ingrese un numero del mes");
 
-    default:
-      while (day < 0 || day > 32) {
-        day = parseInt(
-          prompt("Dia incorrecto, por favor ingrese un numero del mes: ")
-        );
-      }
+    pillbox();
   }
 }
 
-pillbox(day);
+pillbox();
